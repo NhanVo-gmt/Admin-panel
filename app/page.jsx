f8d5407519740aea16627a0f5770319a8365fc64
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-02-27 00:50:47 +1100

    adding tailwind

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='bg-green-500 text-white'>
+      <h1 className='text-6xl'>Homepage</h1>
+      <p>This is the home page</p>
+    </div>
+  )
+}
+
+export default HomePage
