474c7e72d1fd2a8c20fb94b73e998e9fd191e032
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-02-27 11:06:33 +1100

    adding element in main header

--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import {FaBars} from 'react-icons/fa'
 
 const MainHeader = () => {
   return (
-    <div className='bg-white'>
+    <div className='bg-white flex justify-between items-center px-4 h-12 mb-4'>
         <div>Brand</div>
-        <div>toggle btn</div>
+        <div>
+            <FaBars className='cursor-pointer' />
+        </div>
     </div>
   )
 }
