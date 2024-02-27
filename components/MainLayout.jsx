3bcc27541217e7b20452e8eb5d10700b56c298e3
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-02-27 11:19:58 +1100

    adding element in the left panel

--- components/MainLayout.jsx
+++ components/MainLayout.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
-import MainHeader from './MainHeader'
+import React from "react";
+import MainHeader from "./MainHeader";
 
-const MainLayout = ({children}) => {
+import {AiOutlineHome} from "react-icons/ai"
+import {GrProjects} from "react-icons/gr"
+import Link from "next/link";
+
+
+const MainLayout = ({ children }) => {
   return (
-    <div className='bg-gray-100 w-full min-h-screen'>
-        <MainHeader />
-        <h1>MainLayout</h1>
-        <hr />
-        <main>{children}</main>
+    <div className="bg-gray-100 w-full min-h-screen">
+      <MainHeader />
+      <div className="flex justify-start items-start">
+        <aside className="bg-white rounded-lg w-60 p-4">
+          <ul>
+            <li className="flex justify-start items-center hover:bg-blue-200 hover:text-blue-800 rounded-lg p-2 mb-5">
+              <AiOutlineHome className="mr-2" />
+              <Link href="/">Home</Link>
+            </li>
+            <li className="flex justify-start items-center hover:bg-blue-200 hover:text-blue-800 rounded-lg p-2">
+              <GrProjects className="mr-2" />
+              <Link href="/">Projects</Link>
+            </li>
+          </ul>
+        </aside>
+        <main className="flex-1">{children}</main>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
