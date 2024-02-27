8c72a8f17856732d7643dd3c460f4b0f871ea476
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-02-27 11:37:04 +1100

    adding all elements in main layout

--- components/MainLayout.jsx
+++ components/MainLayout.jsx
@@ -3,6 +3,10 @@ import MainHeader from "./MainHeader";
 
 import {AiOutlineHome} from "react-icons/ai"
 import {GrProjects} from "react-icons/gr"
+import { FaAngleRight, FaCheck, FaCheckDouble } from "react-icons/fa";
+import { SiHelpscout } from "react-icons/si"
+import { FiPhoneCall } from "react-icons/fi"
+
 import Link from "next/link";
 
 
@@ -13,13 +17,32 @@ const MainLayout = ({ children }) => {
       <div className="flex justify-start items-start">
         <aside className="bg-white rounded-lg w-60 p-4">
           <ul>
-            <li className="flex justify-start items-center hover:bg-blue-200 hover:text-blue-800 rounded-lg p-2 mb-5">
+            <li className="flex justify-start items-center hover:bg-blue-200 hover:text-blue-800 rounded-lg p-2">
               <AiOutlineHome className="mr-2" />
               <Link href="/">Home</Link>
             </li>
             <li className="flex justify-start items-center hover:bg-blue-200 hover:text-blue-800 rounded-lg p-2">
               <GrProjects className="mr-2" />
-              <Link href="/">Projects</Link>
+              <h3 className="flex-1">Projects</h3>
+              <FaAngleRight />
+            </li>
+            <li className="flex justify-start items-center hover:bg-blue-200 hover:text-blue-800 rounded-lg p-2">
+              <FaCheck className="mr-2" />
+              <h3 className="flex-1">Single</h3>
+              <FaAngleRight />
+            </li>
+            <li className="flex justify-start items-center hover:bg-blue-200 hover:text-blue-800 rounded-lg p-2">
+              <FaCheckDouble className="mr-2" />
+              <h3 className="flex-1">Complex</h3>
+              <FaAngleRight />
+            </li>
+            <li className="flex justify-start items-center hover:bg-blue-200 hover:text-blue-800 rounded-lg p-2">
+              <SiHelpscout className="mr-2" />
+              <Link href="/about-us">About us</Link>
+            </li>
+            <li className="flex justify-start items-center hover:bg-blue-200 hover:text-blue-800 rounded-lg p-2">
+              <FiPhoneCall className="mr-2" />
+              <Link href="/contact-us">Contact us</Link>
             </li>
           </ul>
         </aside>
