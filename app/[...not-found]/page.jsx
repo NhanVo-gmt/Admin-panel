35dd17872447e5965c545c6e2ce761d378e217c9
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-01 18:15:38 +1100

    customise not found page

---
+++ app/[...not-found]/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='flex flex-col items-center justify-center w-full bg-red-400 p-32'>
+        <h1 className='text-red-800 text-8xl mb-4'>Not Found Page</h1>
+        <h2 className='text-2xl'>Please try again or go to the other page</h2>
+    </div>
+  )
+}
+
+export default NotFound
