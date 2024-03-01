f72c67d50efbf234235c28a3a39f3987a0238954
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-01 13:34:55 +1100

    Creating page for checkbox

---
+++ app/singular/selectbox/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SelectBoxPage = () => {
+  return (
+    <div className='bg-white rounded-lg mx-4 p-4'>
+        This is SelectBox page
+    </div>
+  )
+}
+
+export default SelectBoxPage
