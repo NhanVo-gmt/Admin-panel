872de3cc3519c7573ea3fab5a0221925ce723e2c
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-01 17:52:33 +1100

    restructure and adding user area select box

---
+++ components/layout/UserAreaSelectBox.jsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { useState } from "react";
+
+const cities = ["Tehran", "Shiraz", "Yazd", "Mashad", "Esfashan"];
+import { BsChevronExpand } from "react-icons/bs";
+import { AiOutlineCheck } from "react-icons/ai";
+
+const UserAreaSelectBox = () => {
+  const [selectedCity, setSelectedCity] = useState("Tehran");
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <div
+        onClick={() => setOpen((prev) => !prev)}
+        className="bg-gray-100 flex flex-col justify-center items-center w-fit relative z-30"
+      >
+        <div className="flex flex-row justify-between items-center bg-white w-48 my-2 p-2 rounded-lg cursor-pointer">
+          <span>{selectedCity}</span>
+          <BsChevronExpand />
+        </div>
+        <div
+          className={`flex flex-col w-48 my-2 bg-white rounded-lg ${
+            open ? "opacity-100 h-auto" : "opacity-0 h-0 "
+          } overflow-hidden transition-all duration-200 cursor-pointer absolute top-16 left-0`}
+        >
+          {cities.map((item) => (
+            <div
+              onClick={() => {
+                setSelectedCity(item);
+                setOpen(true);
+              }}
+              className={`flex justify-start items-center gap-x-2 px-2 py-1 hover:bg-orange-200 ${
+                selectedCity === item ? "bg-orange-200" : ""
+              }`}
+              key={item}
+            >
+              <AiOutlineCheck
+                className={`text-orange-300 ${
+                  selectedCity === item ? "opacity-100" : "opacity-0"
+                }`}
+              />
+              <span>{item}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={`bg-gray-100 fixed inset-0 opacity-50 z-0 ${open ? "block" : "hidden"}`}></div>
+    </>
+  );
+};
+
+export default UserAreaSelectBox;
