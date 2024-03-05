46f2a7890d82a80f94c144aaab16c54d275f1f7b
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-05 13:32:18 +1100

    adding tailwind scrollbar

--- app/complex/horizontal-scrollbar/page.jsx
+++ app/complex/horizontal-scrollbar/page.jsx
@@ -24,17 +24,38 @@ const categories = [
   "Lifestyle and Vlogs",
 ];
 
+const users = new Array(20).fill(0).map((item, index) => {
+  return {
+    id: index,
+    username: `U#${index}`,
+    image: `https://i.pravatar.cc/150?img=${Math.random() * 50}`,
+  }
+})
+
 const HorizontalScrollbarPage = () => {
   return (
     <div className="bg-white rounded-lg mx-4 p-4">
       <h1>HorizontalScrollbarPage</h1>
       <br />
       <br />
-      <HorizontalScrollbar>
+      <h1 className="text-blue-800 text-2xl">Youtube categories</h1>
+      <HorizontalScrollbar scrollbar={false}>
         {
           categories.map((item, index) => (
             <div key={index} className="border-2 border-gray-200 p-1 shrink-0 rounded-lg hover:border-gray-400">
               <h1>{item}</h1>
+            </div>
+          ))
+        }
+      </HorizontalScrollbar>
+      <br />
+      <br />
+      <h1 className="text-pink-800 text-2xl">Instagram categories</h1>
+      <HorizontalScrollbar>
+        {
+          users.map((item, index) => (
+            <div key={index} className="shrink-0">
+              <img src={item.image} alt={item.username} className="w-14 h-14 rounded-full cursor-pointer hover:scale-125 transition-all duration-200"/>
             </div>
           ))
         }
