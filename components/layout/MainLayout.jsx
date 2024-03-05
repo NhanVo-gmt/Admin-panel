8d7d94f28a7abee3428601309c9057f379490058
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-05 11:08:20 +1100

    refactor the sidebar and the content

--- components/layout/MainLayout.jsx
+++ components/layout/MainLayout.jsx
@@ -3,7 +3,7 @@
 import React, { useContext } from "react";
 import MainHeader from "./MainHeader";
 
-import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineHome, AiOutlineClose } from "react-icons/ai";
 import { GrProjects } from "react-icons/gr";
 import { FaAngleRight, FaCheck, FaCheckDouble } from "react-icons/fa";
 import { SiHelpscout, SiSinglestore } from "react-icons/si";
@@ -13,19 +13,28 @@ import Link from "next/link";
 import { MenuContext } from "@/context/MenuContext";
 
 const MainLayout = ({ children }) => {
-  const { open } = useContext(MenuContext);
+  const { open, toggle } = useContext(MenuContext);
+
+  const closeSideBarHandler = () => {
+    toggle();
+  };
 
   return (
     <div className="bg-green-200 min-h-screen">
       <aside
-        className={`bg-red-200 top-4 left-4 rounded-lg overflow-hidden transition-all duration-150 ${
+        className={`bg-white top-4 left-4 rounded-lg overflow-hidden transition-all duration-150 ${
           open ? "w-60 p-4 block fixed" : "w-0 hidden"
-        } lg:w-60 lg:p-4 lg:fixed lg:top-16 lg:left-8 lg:block z-50`}
+        } lg:w-60 lg:p-4 lg:fixed lg:top-16 lg:left-8 lg:block z-50 shadow-md`}
       >
         <ul>
+          <li className="flex justify-end items-center lg:hidden">
+            <AiOutlineClose className="mr-2 cursor-pointer" onClick={closeSideBarHandler}/>
+          </li>
           <li className="flex justify-start items-center hover:bg-blue-200 hover:text-blue-800 rounded-lg p-2">
             <AiOutlineHome className="mr-2" />
-            <Link href="/">Home</Link>
+            <Link href="/" onClick={closeSideBarHandler}>
+              Home
+            </Link>
           </li>
           <li className="flex justify-start items-center hover:bg-blue-200 hover:text-blue-800 rounded-lg p-2">
             <GrProjects className="mr-2" />
@@ -41,7 +50,9 @@ const MainLayout = ({ children }) => {
             <ul className="ml-8 mt-4">
               <li className="flex justify-center items-center gap-3">
                 <SiSinglestore />
-                <Link href="/singular/selectbox">Select box</Link>
+                <Link href="/singular/selectbox" onClick={closeSideBarHandler}>
+                  Select box
+                </Link>
               </li>
             </ul>
           </li>
@@ -52,15 +63,19 @@ const MainLayout = ({ children }) => {
           </li>
           <li className="flex justify-start items-center hover:bg-blue-200 hover:text-blue-800 rounded-lg p-2">
             <SiHelpscout className="mr-2" />
-            <Link href="/about-us">About us</Link>
+            <Link href="/about-us" onClick={closeSideBarHandler}>
+              About us
+            </Link>
           </li>
           <li className="flex justify-start items-center hover:bg-blue-200 hover:text-blue-800 rounded-lg p-2">
             <FiPhoneCall className="mr-2" />
-            <Link href="/contact-us">Contact us</Link>
+            <Link href="/contact-us" onClick={closeSideBarHandler}>
+              Contact us
+            </Link>
           </li>
         </ul>
       </aside>
-      <div className={`${open ? "blur-lg" : "blur-0"}`}>
+      <div className={`${open ? "max-lg:blur-lg" : "blur-0"}`}>
         <MainHeader />
         <main className="lg:ml-[280px]">{children}</main>
       </div>
