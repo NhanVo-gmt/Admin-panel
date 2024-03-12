7fe61c0ecbfbb1435c595a719c57621cc1a7e1ba
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-03-13 02:06:46 +1100

    function for form data

--- app/complex/simple-multi-step-form/components/SimpleMultiStepForm.jsx
+++ app/complex/simple-multi-step-form/components/SimpleMultiStepForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const initialFormData = {
     firstName: '',
@@ -8,9 +8,55 @@ const initialFormData = {
     businessWebsite: '',
     businessEmail: '',
     incomePerMonth: 0,
+    agreeToTerms: false,
 };
 
+const stepsArray = ['A', 'B', 'C', 'D'];
+
 const SimpleMultiStepForm = ({showStepNumber}) => {
+    const [step, setStep] = useState('A');
+    const [formData, setFormData] = useState(initialFormData);
+
+    const handleNextStep = () => {
+        if (step === 'A') setStep('B')
+        else if (step === 'B') setStep('C')
+        else if (step === 'C') setStep('D')
+    }
+
+    const handlePrevStep = () => {
+        if (step === 'D') setStep('C')
+        else if (step === 'C') setStep('B')
+        else if (step === 'B') setStep('A')
+    }
+
+    //update our form data
+    const handleChangeInput = (event) => {
+        const fieldName = event.target.name;
+        let fieldValue;
+        if (fieldName === 'agreeToTerms')
+            fieldValue = event.target.checked;
+        else 
+            fieldValue = event.target.value;
+
+        setFormData({...formData, [fieldName]: fieldValue})
+    }
+
+    // handle submit form
+    const handleSubmitFormData = () => {
+        if (!formData.agreeToTerms) {
+            alert ('Error!!! You must agree to terms of service')
+        }
+        else 
+        {
+            setStep('Final');
+        }
+    };
+
+    useEffect(() => {
+        console.log(formData);
+    }, [formData])
+
+
   return (
     <div>SimpleMultiStepForm</div>
   )
